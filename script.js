c26332a564d08103d620b4df652d0c4a9e66d729
Naoki Kojima <dark.light9640@gmail.com>
2020-10-24 00:12:11 +0900

    add coading

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-// Get Quote From API
-async function getQuote(){
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQupteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
 }
 
+function removeLodingSpinner(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+async function getQuoteFromAPI(){
+    showLoadingSpinner();
+    // using proxy server in order to avoid cors errors
+    const proxyUrl = 'https://quiet-basin-12745.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unknown'
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if(data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop loader, Show Quote
+        removeLodingSpinner();
+    }catch(err){
+        getQuoteFromAPI();
+    }
+}
+
+// Tweet Quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank');
+}
+
+// Event Listeners
+newQupteBtn.addEventListener('click',getQuoteFromAPI);
+twitterBtn.addEventListener('click',tweetQuote);
+
 // On Load
+getQuoteFromAPI();
